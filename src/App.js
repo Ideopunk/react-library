289921822b2c63f35ab55f9a2317c32cd6f82ea8
Conor Barnes <conorbarnes93@gmail.com>
2020-10-07 16:53:50 -0700

    install firebase, add config module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from "./config/fbConfig"
 
 function App() {
   return (
---
+++ src/config/fbConfig.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCRMxRUqUr9OcBFRsdr0k5CSYCn8jwUVTM",
+	authDomain: "react-library-93b10.firebaseapp.com",
+	databaseURL: "https://react-library-93b10.firebaseio.com",
+	projectId: "react-library-93b10",
+	storageBucket: "react-library-93b10.appspot.com",
+	messagingSenderId: "613694204962",
+	appId: "1:613694204962:web:fa9f221640e0ccf012ee7b",
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
