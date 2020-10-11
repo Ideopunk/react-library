99ebec83ebf91632d4f164a7bb12663b06dd8782
Conor Barnes <conorbarnes93@gmail.com>
2020-10-11 16:16:15 -0700

    Add in the SVGs

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,7 @@
 import React from "react";
+import edit from "../assets/create-outline.svg";
+import trash from "../assets/trash-outline.svg";
+
 
 const Table = (props) => {
 	const booksTable = props.books.map((book) => {
@@ -6,15 +9,16 @@ const Table = (props) => {
 			<div key={book.id} id={book.id} className="row">
 				<div>{book.title}</div>
 				<div>{book.author}</div>
-				<div>{book.pages}</div>
+				<div>{book.pages} pages</div>
 				<div>{book.read}</div>
-                <div className="delete-button" onClick={() => props.handleDelete(book.id)}>x</div>
+				<img src={edit} className="edit-button row-button" onClick={() => props.handleEdit(book.id)} alt="edit"/>
+				<img src={trash} className="delete-button row-button" onClick={() => props.handleDelete(book.id)} alt="trash"/>
 			</div>
 		);
 	});
 	return (
 		<div className="table-container">
-			<h1>Table</h1>
+			<h1 className="table-title">Table</h1>
 			<div className="table">
             	{booksTable}
 			</div>
